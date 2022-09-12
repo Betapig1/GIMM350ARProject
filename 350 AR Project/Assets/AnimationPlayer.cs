098cea577bc1daa92e78c631066090156ba17c9b
Betapig1 <rynahall607@gmail.com>
2022-09-12 13:02:18 -0600

    Core game loop

---
+++ 350 AR Project/Assets/AnimationPlayer.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationPlayer : MonoBehaviour
+{
+    public Animator anim;
+    public Camera cam;
+    public ParticleSystem particle;
+    private bool isUp = false;
+    private float time = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("moveUp", 1, 3);
+    }
+
+    void moveUp()
+    {
+        if (!isUp)
+        {
+            if (Random.Range(0, 100) > 75)
+            {
+                anim.Play("Armature|Spawn", 0);
+                isUp = true;
+                StartCoroutine(goDown());
+            }
+        }
+    }
+
+    IEnumerator goDown()
+    {
+
+
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(time);
+        if (isUp)
+        {
+            anim.Play("Armature|TimeUp", 0);
+            isUp = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Click");
+            var ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                GameObject obj = hit.collider.gameObject;
+                Debug.Log(transform.gameObject.tag);
+                if (obj == transform.gameObject && isUp)
+                {
+                    particle.Play();
+                    anim.Play("Armature|Hit", 0);
+                    isUp = false;
+                }
+            }
+        }
+    }
+}
---
+++ 350 AR Project/Library/Bee/artifacts/Android/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,85 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void AnimationPlayer::Start()
+extern void AnimationPlayer_Start_m58C30483DB3CBC2B3627588C434AFC70BE065303 (void);
+// 0x00000002 System.Void AnimationPlayer::moveUp()
+extern void AnimationPlayer_moveUp_m7513BD0DF683DF37A11470280C03E991C3F55331 (void);
+// 0x00000003 System.Collections.IEnumerator AnimationPlayer::goDown()
+extern void AnimationPlayer_goDown_m4792E996720167793CAF4237834D415CC3948303 (void);
+// 0x00000004 System.Void AnimationPlayer::Update()
+extern void AnimationPlayer_Update_m885543006A8D548C9EB40B752E3CBECF47C10878 (void);
+// 0x00000005 System.Void AnimationPlayer::.ctor()
+extern void AnimationPlayer__ctor_mD79465BDD30167C8B8B506C26A737F38C1A37FB3 (void);
+// 0x00000006 System.Void AnimationPlayer/<goDown>d__7::.ctor(System.Int32)
+extern void U3CgoDownU3Ed__7__ctor_m61992C45040CF2F11C30A3AD8A8084CF945D5605 (void);
+// 0x00000007 System.Void AnimationPlayer/<goDown>d__7::System.IDisposable.Dispose()
+extern void U3CgoDownU3Ed__7_System_IDisposable_Dispose_mFD596C965D5C85344876DC0C7E911CC42B11EDC6 (void);
+// 0x00000008 System.Boolean AnimationPlayer/<goDown>d__7::MoveNext()
+extern void U3CgoDownU3Ed__7_MoveNext_m0E0E4C4E425B027E2F62AB310A76F0D373E404E0 (void);
+// 0x00000009 System.Object AnimationPlayer/<goDown>d__7::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CgoDownU3Ed__7_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m39CBEFD4E0A44AC77ADAEB2F89FF8BE14AEC6107 (void);
+// 0x0000000A System.Void AnimationPlayer/<goDown>d__7::System.Collections.IEnumerator.Reset()
+extern void U3CgoDownU3Ed__7_System_Collections_IEnumerator_Reset_mE8F304A074551634BA7CDB2E410167B4EDDC5905 (void);
+// 0x0000000B System.Object AnimationPlayer/<goDown>d__7::System.Collections.IEnumerator.get_Current()
+extern void U3CgoDownU3Ed__7_System_Collections_IEnumerator_get_Current_m2C700332FB3C236E3ABD96D721FE0B6C8DDD5321 (void);
+static Il2CppMethodPointer s_methodPointers[11] = 
+{
+	AnimationPlayer_Start_m58C30483DB3CBC2B3627588C434AFC70BE065303,
+	AnimationPlayer_moveUp_m7513BD0DF683DF37A11470280C03E991C3F55331,
+	AnimationPlayer_goDown_m4792E996720167793CAF4237834D415CC3948303,
+	AnimationPlayer_Update_m885543006A8D548C9EB40B752E3CBECF47C10878,
+	AnimationPlayer__ctor_mD79465BDD30167C8B8B506C26A737F38C1A37FB3,
+	U3CgoDownU3Ed__7__ctor_m61992C45040CF2F11C30A3AD8A8084CF945D5605,
+	U3CgoDownU3Ed__7_System_IDisposable_Dispose_mFD596C965D5C85344876DC0C7E911CC42B11EDC6,
+	U3CgoDownU3Ed__7_MoveNext_m0E0E4C4E425B027E2F62AB310A76F0D373E404E0,
+	U3CgoDownU3Ed__7_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_m39CBEFD4E0A44AC77ADAEB2F89FF8BE14AEC6107,
+	U3CgoDownU3Ed__7_System_Collections_IEnumerator_Reset_mE8F304A074551634BA7CDB2E410167B4EDDC5905,
+	U3CgoDownU3Ed__7_System_Collections_IEnumerator_get_Current_m2C700332FB3C236E3ABD96D721FE0B6C8DDD5321,
+};
+static const int32_t s_InvokerIndices[11] = 
+{
+	2086,
+	2086,
+	2054,
+	2086,
+	2086,
+	1718,
+	2086,
+	2022,
+	2054,
+	2086,
+	2054,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	11,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
